1ed7958956244ee3718d5e5bfee59bbc8efc8dc2
YahyaDalbah <yahya1dalbah@gmail.com>
2025-05-04 19:31:34 +0300

    applied form filtering

--- src/app/pages/properties/properties.component.ts
+++ src/app/pages/properties/properties.component.ts
@@ -3,10 +3,11 @@ import { Property } from '../../types';
 import { PropertieCardComponent } from '../../components/propertie-card/propertie-card.component';
 import { CommonModule } from '@angular/common';
 import { PropertyService } from '../../sevices/poperty.service';
+import { FormGroup, FormBuilder, ReactiveFormsModule } from '@angular/forms';
 
 @Component({
   selector: 'app-properties',
-  imports: [PropertieCardComponent, CommonModule],
+  imports: [PropertieCardComponent, CommonModule, ReactiveFormsModule],
   templateUrl: './properties.component.html',
   styleUrl: './properties.component.scss',
 })
@@ -15,18 +16,53 @@ export class PropertiesComponent implements OnInit {
   pageSize = 3;
   propertiyService = inject(PropertyService);
   properties: Property[] = [];
+  filteredProperties: Property[] = [];
+  private fb = inject(FormBuilder);
+
+  filterForm = this.fb.group({
+    search: [''],
+    forRent: [false],
+    forSale: [false],
+    bedrooms: [null],
+    bathrooms: [null],
+    minPrice: [null],
+    maxPrice: [null],
+  });
+
+  applyFilters() {
+    const filters = this.filterForm.value;
+    this.filteredProperties = this.properties.filter((property) => {
+      return (
+        (!filters.search ||
+          property.title
+            .toLowerCase()
+            .includes(filters.search.toLowerCase())) &&
+        (!filters.forRent || property.forRent) &&
+        (!filters.forSale || property.forSale) &&
+        (!filters.bedrooms || filters.bedrooms == property.bedrooms) &&
+        (!filters.bathrooms || filters.bathrooms == property.bathrooms) &&
+        (!filters.minPrice || property.price >= filters.minPrice) &&
+        (!filters.maxPrice || property.price <= filters.maxPrice)
+      );
+    });
+    this.currentPage = 1;
+  }
 
   ngOnInit(): void {
     this.properties = this.propertiyService.getProperties();
+    this.applyFilters();
   }
 
   get paginatedProperties() {
     const startIndex = (this.currentPage - 1) * this.pageSize;
-    return this.properties.slice(startIndex, startIndex + this.pageSize);
+    return this.filteredProperties.slice(
+      startIndex,
+      startIndex + this.pageSize
+    );
   }
 
   get totalPages(): number {
-    return Math.ceil(this.properties.length / this.pageSize);
+    return Math.ceil(this.filteredProperties.length / this.pageSize);
   }
 
   pageChanged(newPage: number) {
